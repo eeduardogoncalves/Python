3e4236aaef954c47182332672f75cfbb226e70dc
eeduardogoncalves <82242008+eeduardogoncalves@users.noreply.github.com>
2023-01-29 22:43:03 -0300

    Add files via upload

---
+++ Gerador de senhas Python/Gerador de senhas.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+def gerar_senha(tamanho):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for i in range(tamanho))
+    return senha
+
+print(gerar_senha(12))
